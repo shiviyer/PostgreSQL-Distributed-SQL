206e70141670dd79f67c213f5b0c9bdb8756583b
Shiv Iyer <38718055+shiviyer@users.noreply.github.com>
2023-01-28 21:16:27 +0530

    Update psql-dist-sql.py
    Write a Python script to compute query arrival rate, service and wait time in PostgreSQL in real-time every 2 seconds

--- psql-dist-sql.py
+++ psql-dist-sql.py
@@ -1,1 +1,40 @@
+import psycopg2
+import time
 
+# Connect to the PostgreSQL database
+conn = psycopg2.connect(
+    dbname="your_db_name",
+    user="your_username",
+    password="your_password",
+    host="your_host",
+    port="your_port"
+)
+
+# Create a cursor object
+cur = conn.cursor()
+
+# Run the script in an infinite loop
+while True:
+    # Execute the pg_stat_activity query to retrieve the query arrival rate, service time, and wait time
+    cur.execute("SELECT datname, usename, query, state, waiting, query_start, now() - query_start AS duration FROM pg_stat_activity WHERE state = 'active'")
+
+    # Fetch the results
+    rows = cur.fetchall()
+
+    # Print the results
+    for row in rows:
+        print("Database: ", row[0])
+        print("User: ", row[1])
+        print("Query: ", row[2])
+        print("State: ", row[3])
+        print("Waiting: ", row[4])
+        print("Start Time: ", row[5])
+        print("Duration: ", row[6])
+        print("-------------------")
+
+    # Sleep for 2 seconds
+    time.sleep(2)
+
+# Close the cursor and connection
+cur.close()
+conn.close()
